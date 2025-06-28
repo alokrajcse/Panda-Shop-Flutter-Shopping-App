b7ee930bb5b7f7a654d4c7ee0b01fd2c92493952
alokrajcse <cse.22bcse81@silicon.ac.in>
2025-06-28 14:56:50 +0530

    init

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 
 class Carousel extends StatelessWidget {
   final List<String> featuredImages = [
+    'https://th.bing.com/th/id/R.43d59b97e0257c1ebf9c7a1598720b8c?rik=2l4OgiK3IHgjCQ&riu=http%3a%2f%2fstatic.vectorcharacters.net%2fuploads%2f2012%2f03%2fCute_Panda_Cartoon_Character.jpg&ehk=7tBdKLp%2f5uAA8x%2f6EM9VtDA4IGJALFVOC27WpMagvD0%3d&risl=&pid=ImgRaw&r=0',
     'https://sj.sunderland.ac.uk/images/internal-websites/sls/student-financial-advice/panda-banner-long.png',
     'https://tse1.mm.bing.net/th/id/OIP.pqbH-9rP_ME-4k1qlMMaBgHaE8?rs=1&pid=ImgDetMain&cb=idpwebp2&o=7&rm=3',
-    'https://th.bing.com/th/id/R.43d59b97e0257c1ebf9c7a1598720b8c?rik=2l4OgiK3IHgjCQ&riu=http%3a%2f%2fstatic.vectorcharacters.net%2fuploads%2f2012%2f03%2fCute_Panda_Cartoon_Character.jpg&ehk=7tBdKLp%2f5uAA8x%2f6EM9VtDA4IGJALFVOC27WpMagvD0%3d&risl=&pid=ImgRaw&r=0',
+
   ];
 
   @override
